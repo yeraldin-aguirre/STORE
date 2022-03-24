391b420eec581ea7e05b5a796959c37b16b63468
yeraldin-aguirre <ye.agmi@gmail.com>
2022-03-24 14:01:58 -0500

    config inicial

---
+++ DataBase/store2.sql
@@ -0,0 +1,302 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 23-03-2022 a las 23:58:49
+-- Versión del servidor: 10.4.21-MariaDB
+-- Versión de PHP: 8.0.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `store`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `administrador`
+--
+
+CREATE TABLE `administrador` (
+  `id` int(7) NOT NULL,
+  `Nombre` varchar(30) NOT NULL,
+  `Clave` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `administrador`
+--
+
+INSERT INTO `administrador` (`id`, `Nombre`, `Clave`) VALUES
+(1, 'admin', '21232f297a57a5a743894a0e4a801fc3'),
+(2, 'pepito', '25d55ad283aa400af464c76d713c07ad');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `categoria`
+--
+
+CREATE TABLE `categoria` (
+  `CodigoCat` varchar(30) NOT NULL,
+  `Nombre` varchar(30) NOT NULL,
+  `Descripcion` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `categoria`
+--
+
+INSERT INTO `categoria` (`CodigoCat`, `Nombre`, `Descripcion`) VALUES
+('01', 'Vitrinas de lujo', 'Todo vitrinas de lujo');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `cliente`
+--
+
+CREATE TABLE `cliente` (
+  `NIT` varchar(30) NOT NULL,
+  `Nombre` varchar(30) NOT NULL,
+  `NombreCompleto` varchar(70) NOT NULL,
+  `Apellido` varchar(70) NOT NULL,
+  `Clave` text NOT NULL,
+  `Direccion` varchar(200) NOT NULL,
+  `Telefono` varchar(20) NOT NULL,
+  `Email` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `cliente`
+--
+
+INSERT INTO `cliente` (`NIT`, `Nombre`, `NombreCompleto`, `Apellido`, `Clave`, `Direccion`, `Telefono`, `Email`) VALUES
+('234124', 'carloslop', 'carlos', 'lopez', '25d55ad283aa400af464c76d713c07ad', 'calle 10', '32432423', 'cliente@gmail.com'),
+('546457457', 'carloslo', 'carlos', 'lopez', '25d55ad283aa400af464c76d713c07ad', 'calle 10', '35345345', 'carlos@carlos.com');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `cuentabanco`
+--
+
+CREATE TABLE `cuentabanco` (
+  `id` int(50) NOT NULL,
+  `NumeroCuenta` varchar(100) NOT NULL,
+  `NombreBanco` varchar(100) NOT NULL,
+  `NombreBeneficiario` varchar(100) NOT NULL,
+  `TipoCuenta` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `cuentabanco`
+--
+
+INSERT INTO `cuentabanco` (`id`, `NumeroCuenta`, `NombreBanco`, `NombreBeneficiario`, `TipoCuenta`) VALUES
+(1, '3453464564', 'Colpatria', 'Superadmin', 'ahorros');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `detalle`
+--
+
+CREATE TABLE `detalle` (
+  `NumPedido` int(20) NOT NULL,
+  `CodigoProd` varchar(30) NOT NULL,
+  `CantidadProductos` int(20) NOT NULL,
+  `PrecioProd` decimal(30,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `producto`
+--
+
+CREATE TABLE `producto` (
+  `id` int(20) NOT NULL,
+  `CodigoProd` varchar(30) NOT NULL,
+  `NombreProd` varchar(30) NOT NULL,
+  `CodigoCat` varchar(30) NOT NULL,
+  `Precio` decimal(30,2) NOT NULL,
+  `Descuento` int(2) NOT NULL,
+  `Modelo` varchar(30) NOT NULL,
+  `Marca` varchar(30) NOT NULL,
+  `Stock` int(20) NOT NULL,
+  `NITProveedor` varchar(30) NOT NULL,
+  `Imagen` varchar(150) NOT NULL,
+  `Nombre` varchar(30) NOT NULL,
+  `Estado` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `producto`
+--
+
+INSERT INTO `producto` (`id`, `CodigoProd`, `NombreProd`, `CodigoCat`, `Precio`, `Descuento`, `Modelo`, `Marca`, `Stock`, `NITProveedor`, `Imagen`, `Nombre`, `Estado`) VALUES
+(4, '04', 'vitrina de lujo', '01', '500000.00', 5, 'basico', 'deluxe', 45, '3534534534', '04.jpg', 'admin', 'Activo'),
+(5, '05', 'vitrina panaderia', '01', '300000.00', 0, 'basico', 'royal', 30, '3534534534', '05.jpg', 'admin', 'Activo');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `proveedor`
+--
+
+CREATE TABLE `proveedor` (
+  `NITProveedor` varchar(30) NOT NULL,
+  `NombreProveedor` varchar(30) NOT NULL,
+  `Direccion` varchar(200) NOT NULL,
+  `Telefono` varchar(20) NOT NULL,
+  `PaginaWeb` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `proveedor`
+--
+
+INSERT INTO `proveedor` (`NITProveedor`, `NombreProveedor`, `Direccion`, `Telefono`, `PaginaWeb`) VALUES
+('3534534534', 'LA14', 'Calle 10', '3253253534', '');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `venta`
+--
+
+CREATE TABLE `venta` (
+  `NumPedido` int(20) NOT NULL,
+  `Fecha` varchar(150) NOT NULL,
+  `NIT` varchar(30) NOT NULL,
+  `TotalPagar` decimal(30,2) NOT NULL,
+  `Estado` varchar(150) NOT NULL,
+  `NumeroDeposito` varchar(50) NOT NULL,
+  `TipoEnvio` varchar(37) NOT NULL,
+  `Adjunto` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `administrador`
+--
+ALTER TABLE `administrador`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `categoria`
+--
+ALTER TABLE `categoria`
+  ADD PRIMARY KEY (`CodigoCat`);
+
+--
+-- Indices de la tabla `cliente`
+--
+ALTER TABLE `cliente`
+  ADD PRIMARY KEY (`NIT`);
+
+--
+-- Indices de la tabla `cuentabanco`
+--
+ALTER TABLE `cuentabanco`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `detalle`
+--
+ALTER TABLE `detalle`
+  ADD KEY `NumPedido` (`NumPedido`),
+  ADD KEY `CodigoProd` (`CodigoProd`);
+
+--
+-- Indices de la tabla `producto`
+--
+ALTER TABLE `producto`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `CodigoCat` (`CodigoCat`),
+  ADD KEY `NITProveedor` (`NITProveedor`),
+  ADD KEY `Agregado` (`Nombre`);
+
+--
+-- Indices de la tabla `proveedor`
+--
+ALTER TABLE `proveedor`
+  ADD PRIMARY KEY (`NITProveedor`);
+
+--
+-- Indices de la tabla `venta`
+--
+ALTER TABLE `venta`
+  ADD PRIMARY KEY (`NumPedido`),
+  ADD KEY `NIT` (`NIT`),
+  ADD KEY `NIT_2` (`NIT`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `administrador`
+--
+ALTER TABLE `administrador`
+  MODIFY `id` int(7) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `cuentabanco`
+--
+ALTER TABLE `cuentabanco`
+  MODIFY `id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `producto`
+--
+ALTER TABLE `producto`
+  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT de la tabla `venta`
+--
+ALTER TABLE `venta`
+  MODIFY `NumPedido` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `detalle`
+--
+ALTER TABLE `detalle`
+  ADD CONSTRAINT `detalle_ibfk_9` FOREIGN KEY (`NumPedido`) REFERENCES `venta` (`NumPedido`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `producto`
+--
+ALTER TABLE `producto`
+  ADD CONSTRAINT `producto_ibfk_7` FOREIGN KEY (`CodigoCat`) REFERENCES `categoria` (`CodigoCat`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `producto_ibfk_8` FOREIGN KEY (`NITProveedor`) REFERENCES `proveedor` (`NITProveedor`) ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `venta`
+--
+ALTER TABLE `venta`
+  ADD CONSTRAINT `venta_ibfk_1` FOREIGN KEY (`NIT`) REFERENCES `cliente` (`NIT`) ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ inc/footer.php
@@ -0,0 +1,45 @@
+<div class="ResForm"></div>
+<div class="ResbeforeSend"></div>
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-4">
+                <h4 class="text-footer">Contactanos</h4>
+                <a href="#" target="_blank">
+                    <i class="fa fa-facebook" aria-hidden="true">&nbsp; Facebook </i> 
+                </a><br>
+                <a href="#" target="_blank">
+                    <i class="fa fa-twitter" aria-hidden="true">&nbsp; Twitter </i>
+                </a><br>
+                <a href="#" target="_blank">
+                    <i class="fa fa-youtube-play" aria-hidden="true">&nbsp; YouTube </i>
+                </a><br>
+                <a href="#" target="_blank">
+                    <i class="fa fa-instagram" aria-hidden="true">&nbsp; Instagram </i>
+                </a><br>
+                <a href="#" target="_blank">
+                    <i class="fa fa-map-marker" aria-hidden="true">&nbsp; Encuentranos </i>
+                </a>
+            </div>
+            <div class="col-sm-4">
+                <h4 class="text-footer">¿Porque elegirnos?</h4>
+                <p> 
+                    MAX VITRINAS INFO</br>
+                    MAX VITRINAS INFO</br>
+                    MAX VITRINAS INFO</br>
+                    MAX VITRINAS INFO</br>
+                </p>
+            </div>
+            <div class="col-sm-4">
+                <h4 class="text-footer" >Direccion</h4>
+                <p style="color: #FFF">Cali, Valle</p>
+                <p style="color: #FFF">Carrera ..</p>
+                <p style="font-size:20px" aria-hidden="true">  000 – 000000 / 000 – 000000</p>
+                <p class="fa fa-mobile" style="font-size:20px"  aria-hidden="true">  00000000 / 0000000</p> <a href="#" target="_blank" style="color: #5bc0de">envianos_un_mensaje</a></br>
+                E-mail: <a href="#" target="_blank" style="color: #5bc0de"> &nbsp; ejemplod@dominio.com</a>
+            </div>
+        </div>
+    </div>
+    <br><br><br>
+    <h5 class="text-center tittles-pages-logo text-footer">TPS 1 110 &copy; 2022</h5>
+</footer>
