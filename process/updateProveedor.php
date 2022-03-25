eba8e2f7839a508460ae48aa61fcd078a864fb85
DiegoVega <ferzux.09@gmail.com>
2022-03-25 11:17:54 -0500

    código carpeta process: indentado.

--- process/updateProveedor.php
+++ process/updateProveedor.php
@@ -2,33 +2,33 @@
 include '../library/configServer.php';
 include '../library/consulSQL.php';
 
-$nitOldProveUp=consultasSQL::clean_string($_POST['nit-prove-old']);
-$nameProveUp=consultasSQL::clean_string($_POST['prove-name']);
-$dirProveUp=consultasSQL::clean_string($_POST['prove-dir']);
-$telProveUp=consultasSQL::clean_string($_POST['prove-tel']);
-$webProveUp=consultasSQL::clean_string($_POST['prove-web']);
+$nitOldProveUp = consultasSQL::clean_string($_POST['nit-prove-old']);
+$nameProveUp = consultasSQL::clean_string($_POST['prove-name']);
+$dirProveUp = consultasSQL::clean_string($_POST['prove-dir']);
+$telProveUp = consultasSQL::clean_string($_POST['prove-tel']);
+$webProveUp = consultasSQL::clean_string($_POST['prove-web']);
 
-if(consultasSQL::UpdateSQL("proveedor", "NombreProveedor='$nameProveUp',Direccion='$dirProveUp',Telefono='$telProveUp',PaginaWeb='$webProveUp'", "NITProveedor='$nitOldProveUp'")){
-    echo '<script>
+if (consultasSQL::UpdateSQL("proveedor", "NombreProveedor='$nameProveUp',Direccion='$dirProveUp',Telefono='$telProveUp',PaginaWeb='$webProveUp'", "NITProveedor='$nitOldProveUp'")) {
+	echo '<script>
         swal({
-          title: "Proveedor actualizado",
-          text: "Los datos del proveedor se actualizaron correctamente",
-          type: "success",
-          showCancelButton: true,
-          confirmButtonClass: "btn-danger",
-          confirmButtonText: "Aceptar",
-          cancelButtonText: "Cancelar",
-          closeOnConfirm: false,
-          closeOnCancel: false
-          },
-          function(isConfirm) {
-          if (isConfirm) {
-            location.reload();
-          } else {
-            location.reload();
-          }
-        });
-    </script>';
-}else{
-    echo '<script>swal("ERROR", "Ocurrió un error inesperado, por favor intente nuevamente", "error");</script>';
+			title: "Proveedor actualizado",
+			text: "Los datos del proveedor se actualizaron correctamente",
+			type: "success",
+			showCancelButton: true,
+			confirmButtonClass: "btn-danger",
+			confirmButtonText: "Aceptar",
+			cancelButtonText: "Cancelar",
+			closeOnConfirm: false,
+			closeOnCancel: false
+		},
+			function(isConfirm) {
+			if (isConfirm) {
+				location.reload();
+			} else {
+				location.reload();
+			}
+		});
+		</script>';
+} else {
+	echo '<script>swal("ERROR", "Ocurrió un error inesperado, por favor intente nuevamente", "error");</script>';
 }
