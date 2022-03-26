ae78a2b3f6dd4d57ff5557fe9be2f842547b16a1
DiegoVega <ferzux.09@gmail.com>
2022-03-26 18:15:16 -0500

    etiqueta de base de datos

--- library/configServer.php
+++ library/configServer.php
@@ -1,10 +1,12 @@
 <?php
-// define("USER", "root");
-// define("SERVER", "localhost");
-// define("BD", "store");
-// define("PASS", "");
+// local
+define("USER", "root");
+define("SERVER", "localhost");
+define("BD", "store");
+define("PASS", "");
 
-define("USER", "uslyawznl5wplrxe");
-define("SERVER", "bmfea3uytnohksj2s0ii-mysql.services.clever-cloud.com");
-define("BD", "bmfea3uytnohksj2s0ii");
-define("PASS", "fftUDqr0VBlDe7ZgW1Mw");
+// nube
+// define("USER", "uslyawznl5wplrxe");
+// define("SERVER", "bmfea3uytnohksj2s0ii-mysql.services.clever-cloud.com");
+// define("BD", "bmfea3uytnohksj2s0ii");
+// define("PASS", "fftUDqr0VBlDe7ZgW1Mw");
