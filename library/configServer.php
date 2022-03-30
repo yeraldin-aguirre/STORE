dfeb8ddfee9a1168a8e1c221a2a2f8570b0580bb
diego <ferzux.09@gmail.com>
2022-03-30 16:06:47 -0500

    base de datos nube 2

--- library/configServer.php
+++ library/configServer.php
@@ -5,8 +5,14 @@ define("SERVER", "localhost");
 define("BD", "store");
 define("PASS", "");
 
-// nube
+// nube 1
 // define("USER", "uslyawznl5wplrxe");
 // define("SERVER", "bmfea3uytnohksj2s0ii-mysql.services.clever-cloud.com");
 // define("BD", "bmfea3uytnohksj2s0ii");
 // define("PASS", "fftUDqr0VBlDe7ZgW1Mw");
+
+// nube 2
+// define("USER", "uj1tzajfm4lzmsmq");
+// define("SERVER", "b3zglthoxjvvjfskgqqs-mysql.services.clever-cloud.com");
+// define("BD", "b3zglthoxjvvjfskgqqs");
+// define("PASS", "Tdl9MsVGVVnNFvPVDddW");
