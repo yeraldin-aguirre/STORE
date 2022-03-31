e36498d752e0b31d5f4a6b63d93b675ca8a16f3e
diego-vega <ferzux.09@gmail.com>
2022-03-31 14:14:13 -0500

    cambio bd nube 2

--- library/configServer.php
+++ library/configServer.php
@@ -1,9 +1,9 @@
 <?php
 // local
-define("USER", "root");
-define("SERVER", "localhost");
-define("BD", "store");
-define("PASS", "");
+// define("USER", "root");
+// define("SERVER", "localhost");
+// define("BD", "store");
+// define("PASS", "");
 
 // nube 1
 // define("USER", "uslyawznl5wplrxe");
@@ -12,7 +12,7 @@ define("PASS", "");
 // define("PASS", "fftUDqr0VBlDe7ZgW1Mw");
 
 // nube 2
-// define("USER", "uj1tzajfm4lzmsmq");
-// define("SERVER", "b3zglthoxjvvjfskgqqs-mysql.services.clever-cloud.com");
-// define("BD", "b3zglthoxjvvjfskgqqs");
-// define("PASS", "Tdl9MsVGVVnNFvPVDddW");
+define("USER", "uj1tzajfm4lzmsmq");
+define("SERVER", "b3zglthoxjvvjfskgqqs-mysql.services.clever-cloud.com");
+define("BD", "b3zglthoxjvvjfskgqqs");
+define("PASS", "Tdl9MsVGVVnNFvPVDddW");
